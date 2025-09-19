0a863da937a6d767e9c82cd87756898aec59686d
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-19 12:30:49 +0530

    T01: Number conversion game

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,25 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T01 branch.
+// Program to display integer input in binary and hexadecimal number systems.
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      do {
+         Console.Clear ();
+         Console.Write ("Input: ");
+         try {
+            var input = Convert.ToInt32 (Console.ReadLine ());
+            Console.WriteLine ("HEX: " + Convert.ToString (input, 16).ToUpper ());
+            Console.WriteLine ("Binary: " + Convert.ToString (input, 2));
+         } catch (Exception ex) {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine ("Error: " + ex.Message);
+            Console.ResetColor ();
+         }
+         Console.WriteLine ("Press 'Y' to continue");
+      } while (Console.ReadKey (true).Key == ConsoleKey.Y);
    }
 }
