f28be007c9f03da1307806f5f4763fa54875a0ba
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-19 18:24:04 +0530

    T1: Number conversion game

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,6 +8,19 @@
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      do {
+         Console.Clear ();
+         Console.Write ("Input: ");
+         try {
+            var input = Convert.ToInt32 (Console.ReadLine ());
+            Console.WriteLine ("HEX: " + Convert.ToString (input, 16).ToUpper ());
+            Console.WriteLine ("Binary: " + Convert.ToString (input, 2));
+         } catch (Exception ex) {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine ("Error: " + ex.Message);
+            Console.ResetColor ();
+         }
+         Console.WriteLine ("Press 'Y' to continue");
+      } while (Console.ReadKey (true).Key == ConsoleKey.Y);
    }
 }
