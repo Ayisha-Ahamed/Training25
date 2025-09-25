8458a76c0908723dbb764a483d1eadd5b1e5a5d3
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-25 17:10:55 +0530

    T5: Multiplication Tables

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,16 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T5 branch.
+// Program to print multiplication tables from 1-10.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      for (int i = 1; i < 11; i++, WriteLine ())
+         for (int j = 1; j < 11; j++)
+            Console.WriteLine ($" {i,2} * {j,2} = {i * j,-2}");
    }
 }
