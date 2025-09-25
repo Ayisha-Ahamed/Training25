59c381f127ef3fb4ecafc920719f9baef27ad17c
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-25 16:37:26 +0530

    T4: Chess board

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,37 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T4 branch.
+// Program to print chess board.
 // ------------------------------------------------------------------------------------------------
+using System.Text;
+using static System.Console;
 namespace Training25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      OutputEncoding = new UnicodeEncoding ();
+      string[] whitePcs = { "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖" },
+         blackPcs = { "♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜" };
+      string whitePawn = "♙", blackPawn = "♟";
+      WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
+      PrintChars (blackPcs);
+      WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫");
+      PrintPcs (blackPawn);
+      for (int i = 4; i > 0; i--) PrintPcs ();
+      PrintPcs (whitePawn);
+      PrintChars (whitePcs);
+      WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛");
+   }
+
+   static void PrintPcs (string piece = " ") {
+      Write ("┃");
+      for (int i = 0; i < 8; i++) Write ($" {piece} ┃");
+      WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫");
+   }
+
+   static void PrintChars (string[] chars) {
+      Write ("┃");
+      for (int i = 0; i < 8; i++) Write ($" {chars[i]} ┃");
    }
 }
