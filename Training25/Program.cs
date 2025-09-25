8f8e0a484abc5182559bad4cb1abc6c636f9ac36
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-25 15:21:23 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -15,22 +15,21 @@ internal class Program {
       do {
          Clear ();
          Write ("Input: ");
-         if (int.TryParse (ReadLine (), out int num) && num >= 0) {
-            WriteLine ("BIN: " + ConvertToBase (2, num));
-            WriteLine ("HEX: " + ConvertToBase (16, num));
-         } else
-            WriteLine ("Please enter a positive integer!");
+         if (int.TryParse (ReadLine (), out int num) && num > 0)
+            WriteLine ($"BIN: {ConvertToBase (2, num)}\nHEX: {ConvertToBase (16, num)}");
+         else WriteLine ("Please enter a positive integer!");
          WriteLine ("Press 'Y' to continue");
       } while (ReadKey (true).Key == ConsoleKey.Y);
    }
 
    /// <summary>Returns the string of bits converted from decimal to base number system</summary>
-   public static string ConvertToBase (int numBase, int numVal) {
-      StringBuilder numConBuilder = new ();
-      int logTwo = (int)Math.Log2 (numBase);
-      for (; numVal > 0; numVal >>= logTwo)
-         numConBuilder.Append (ConvertToHexBit (numVal & (numBase - 1)));
-      return numConBuilder.Length == 0 ? "0" : new string (numConBuilder.ToString ().Reverse ().ToArray ());
+   public static string ConvertToBase (int cBase, int val) {
+      if (val == 0) return "0";
+      StringBuilder builder = new ();
+      int logTwo = (int)Math.Log2 (cBase);
+      for (; val > 0; val >>= logTwo)
+         builder.Append (ConvertToHexBit (val & (cBase - 1)));
+      return new string ([.. builder.ToString ().Reverse ()]);
    }
 
    /// <summary>Returns character bit corresponsding to decimal number</summary>
