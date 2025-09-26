f99eb6582c6de7bbbe4b21262d1d5c6d6b1467b5
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-26 10:48:08 +0530

    T6: Implemented method to find digital root

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,33 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T6 branch.
+// Program to find the digital root of a number.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      do {
+         Clear ();
+         Write ("Enter input: ");
+         if (int.TryParse (ReadLine (), out int num))
+            WriteLine ($"Digital root of {num} is {DigitalRoot (num)}");
+         else WriteLine ("Please enter an integer!");
+         WriteLine ("Press 'Y' to continue");
+      } while (ReadKey ().Key == ConsoleKey.Y);
+   }
+
+   /// <summary>Returns the digital root of the input.</summary>
+   static int DigitalRoot (int num) {
+      int sum = 0;
+      num.ToString ().ToCharArray ().Sum (x => sum += (x - '0'));
+      while (sum > 9) {
+         int temp = 0;
+         sum.ToString ().ToCharArray ().Sum (x => temp += (x - '0'));
+         sum = temp;
+      }
+      return sum;
    }
 }
