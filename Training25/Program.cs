3d341ca16887d3be9ec4f310a189ef25ff7037d1
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-27 17:39:45 +0530

    T8: Strong Password

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,38 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T8 branch.
+// Program to check if a pasword is strong.
 // ------------------------------------------------------------------------------------------------
+using System.Text.RegularExpressions;
+using static System.Console;
 namespace Training25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      do {
+         Clear ();
+         Write ("Enter password: ");
+         var input = ReadLine ();
+         if (!String.IsNullOrEmpty (input)) {
+            var regex = new Regex ("^(?!.*\\s)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()-+]).+$"
+               + "^.{8,}$");
+            if (regex.IsMatch (input))
+               WriteLine ("Password is valid");
+            else {
+               if (input.Length < 8) WriteLine ("Password must have atleast 8 characters.");
+               if (!input.ToCharArray ().Any (x => (x >= 'a' && x <= 'z')))
+                  WriteLine ("Password must have atleast one upper case character.");
+               if (!input.ToCharArray ().Any (x => (x >= 'A' && x <= 'Z')))
+                  WriteLine ("Password must have atleast one lower case character.");
+               if (!input.ToCharArray ().Any (x => (x >= '0' && x <= '9')))
+                  WriteLine ("Password must have atleast one numeric character.");
+               if (!Regex.IsMatch (input, @"(?=.*[!@#$%^&*()-+)])"))
+                  WriteLine ("Password must have atleast one special character[!@#$%^&*()-+].");
+               if (input.Contains (' ')) WriteLine ("Password should not have any space characters");
+            }
+         } else WriteLine ("Please enter a valid string!");
+         WriteLine ("Press 'Y' to continue");
+      } while (ReadKey (true).Key == ConsoleKey.Y);
    }
 }
