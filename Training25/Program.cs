89eebe0a509ff4519590a670894fdd39c12ebc6d
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-27 17:47:50 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -13,6 +13,6 @@ internal class Program {
    static void Main (string[] args) {
       for (int i = 1; i < 11; i++, WriteLine ())
          for (int j = 1; j < 11; j++)
-            Console.WriteLine ($" {i,2} * {j,2} = {i * j,-2}");
+            WriteLine ($" {i,2} * {j,2} = {i * j,-2}");
    }
 }
