0a133c13024a11b2ab823da287069e133115b88b
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-29 18:51:58 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -10,32 +10,26 @@ using static System.Console;
 namespace Training25;
 
 internal class Program {
-   static void Main (string[] args) {
+   static void Main () {
       do {
          Clear ();
          WriteLine ("Enter two positive integers: ");
-         if (int.TryParse (ReadLine (), out int num1) && int.TryParse (ReadLine (), out int num2)) {
-            int gcd = GCD (num1, num2);
-            WriteLine ($"GCD of {num1} and {num2} is {gcd}");
-            WriteLine ($"LCM of {num1} and {num2} is {LCM (gcd, num1, num2)}");
-         } else
-            WriteLine ("Please enter positive integers!");
+         if (int.TryParse (ReadLine (), out int num1) && num1 > 0 &&
+            int.TryParse (ReadLine (), out int num2) && num2 > 0) {
+            WriteLine ($"GCD of {num1} and {num2} is {GCD (num1, num2)}");
+            WriteLine ($"LCM of {num1} and {num2} is {LCM (num1, num2)}");
+         } else WriteLine ("Please enter positive integers!");
          WriteLine ("Press 'Y' to continue");
       }
       while (ReadKey (true).Key == ConsoleKey.Y);
    }
 
-   /// <summary>Returns GCD of two positive integers.</summary>
+   /// <summary>Returns GCD of two positive integers</summary>
    static int GCD (int num1, int num2) {
-      int a = num1 + num2 - Math.Min (num1, num2), b = num1 + num2 - a;
-      while (b != 0) {
-         int r = a % b;
-         a = b;
-         b = r;
-      }
-      return a;
+      do (num1, num2) = (num2, num1 % num2); while (num2 != 0);
+      return num1;
    }
 
-   /// <summary>Returns the LCM of two positive integers.</summary>
-   static int LCM (int gcd, int num1, int num2) => (num1 * num2) / gcd;
+   /// <summary>Returns LCM of two positive integers</summary>
+   static int LCM (int num1, int num2) => (num1 * num2) / GCD (num1, num2);
 }
