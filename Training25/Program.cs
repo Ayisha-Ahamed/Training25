fe9dc958b0ca2992f429105628cbe429dcbd9e8c
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-30 17:54:56 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -13,23 +13,19 @@ namespace Training25;
 internal class Program {
    static void Main () {
       OutputEncoding = new UnicodeEncoding ();
-      char[] whitePcs = ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
-      blackPcs = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'];
-      char whitePawn = '♙', blackPawn = '♟';
-      for (int i = 0; i < 17; i++) switch (i) {
+      for (int i = 0; i < 17; i++)
+         switch (i) {
             case 0: WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓"); break;
-            case 1: PrintChars (blackPcs); break;
-            case 3: PrintChars ([.. Enumerable.Repeat (blackPawn, 8)]); break;
+            case 1: PrintChars (['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜']); break;
+            case 3: PrintChars ([.. Enumerable.Repeat ('♟', 8)]); break;
             case 5 or 7 or 9 or 11: PrintChars ([.. Enumerable.Repeat (' ', 8)]); break;
-            case 13: PrintChars ([.. Enumerable.Repeat (whitePawn, 8)]); break;
-            case 15: PrintChars (whitePcs); break;
+            case 13: PrintChars ([.. Enumerable.Repeat ('♙', 8)]); break;
+            case 15: PrintChars (['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']); break;
             case 16: WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛"); break;
             default: WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫"); break;
          }
    }
 
-   static void PrintChars (char[] pieces) {
-      Write ("┃");
-      for (int i = 0; i < 8; i++) Write ($" {pieces[i]} ┃");
-   }
+   static void PrintChars (char[] pieces) =>
+      Write ("┃ " + string.Join (" ┃ ", pieces) + " ┃");
 }
