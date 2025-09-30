708807653058afdb2aef12c4d3a47d168fb69f22
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-09-30 10:43:52 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -11,29 +11,25 @@ using static System.Console;
 namespace Training25;
 
 internal class Program {
-   static void Main (string[] args) {
+   static void Main () {
       OutputEncoding = new UnicodeEncoding ();
-      string[] whitePcs = { "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖" },
-         blackPcs = { "♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜" };
-      string whitePawn = "♙", blackPawn = "♟";
-      WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
-      PrintChars (blackPcs);
-      WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫");
-      PrintPcs (blackPawn);
-      for (int i = 4; i > 0; i--) PrintPcs ();
-      PrintPcs (whitePawn);
-      PrintChars (whitePcs);
-      WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛");
+      char[] whitePcs = ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
+      blackPcs = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'];
+      char whitePawn = '♙', blackPawn = '♟';
+      for (int i = 0; i < 17; i++) switch (i) {
+            case 0: WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓"); break;
+            case 1: PrintChars (blackPcs); break;
+            case 3: PrintChars ([.. Enumerable.Repeat (blackPawn, 8)]); break;
+            case 5 or 7 or 9 or 11: PrintChars ([.. Enumerable.Repeat (' ', 8)]); break;
+            case 13: PrintChars ([.. Enumerable.Repeat (whitePawn, 8)]); break;
+            case 15: PrintChars (whitePcs); break;
+            case 16: WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛"); break;
+            default: WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫"); break;
+         }
    }
 
-   static void PrintPcs (string piece = " ") {
+   static void PrintChars (char[] pieces) {
       Write ("┃");
-      for (int i = 0; i < 8; i++) Write ($" {piece} ┃");
-      WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫");
-   }
-
-   static void PrintChars (string[] chars) {
-      Write ("┃");
-      for (int i = 0; i < 8; i++) Write ($" {chars[i]} ┃");
+      for (int i = 0; i < 8; i++) Write ($" {pieces[i]} ┃");
    }
 }
