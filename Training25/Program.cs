21791efbb6189c996a90eb910ad580f61c27ec9a
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-03 11:15:01 +0530

    T9: Reduced string

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,35 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T9 branch.
+// Program to reduce string by removing pairs of adjacent letters.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      do {
+         Clear ();
+         Write ("Enter input: ");
+         var input = ReadLine ();
+         if (!String.IsNullOrEmpty (input) && input.All (a => char.IsLetter (a)))
+            WriteLine ("Output: " + ReducedString (input));
+         else WriteLine ("Please enter a valid input!");
+         Write ("Press 'Y' to continue");
+
+      } while (ReadKey (true).Key == ConsoleKey.Y);
+   }
+
+   static string ReducedString (string input) {
+      int i = 0;
+      string output = "";
+      // Process input string up to second last character.
+      while (i < input.Length - 1) {
+         if (input[i] == input[i + 1]) i += 2;
+         else output += input[i++];
+      }
+      if (i == input.Length - 1) output += input[i];
+      return output;
    }
 }
