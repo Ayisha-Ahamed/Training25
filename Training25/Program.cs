36fb4cdf1a00920f7e6485573138b8db728fbea5
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-08 22:33:47 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,8 +3,8 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on T4 branch.
-// Program to print chess board.
+// Program on main branch.
+// T4: Program to print chess board.
 // ------------------------------------------------------------------------------------------------
 using System.Text;
 using static System.Console;
@@ -13,19 +13,22 @@ namespace Training25;
 internal class Program {
    static void Main () {
       OutputEncoding = new UnicodeEncoding ();
-      for (int i = 0; i < 17; i++)
-         switch (i) {
-            case 0: WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓"); break;
-            case 1: PrintChars (['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜']); break;
-            case 3: PrintChars ([.. Enumerable.Repeat ('♟', 8)]); break;
-            case 5 or 7 or 9 or 11: PrintChars ([.. Enumerable.Repeat (' ', 8)]); break;
-            case 13: PrintChars ([.. Enumerable.Repeat ('♙', 8)]); break;
-            case 15: PrintChars (['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']); break;
-            case 16: WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛"); break;
-            default: WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫"); break;
+      WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
+      PrintRow ("♜♞♝♛♚♝♞♜");
+      char[] pawns = { '♟', '♙' };
+      for (int i = 0; i < 13; i++)
+         // In even iterations print line separator.
+         if ((i & 1) == 0) WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫");
+         else {
+            char rChar = ' ';
+            // Print black or white pawns for i = 1 or i = 11 respectively.
+            if (i % 10 == 1) rChar = pawns[0 + (i / 10)];
+            PrintRow (string.Concat (Enumerable.Repeat (rChar, 8)));
          }
+      PrintRow ("♖♘♗♕♔♗♘♖");
+      WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛");
    }
 
-   static void PrintChars (char[] pieces) =>
-      Write ("┃ " + string.Join (" ┃ ", pieces) + " ┃");
+   static void PrintRow (string pieces) =>
+      Write ("┃ " + string.Join (" ┃ ", pieces.ToCharArray ()) + " ┃");
 }
