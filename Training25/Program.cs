be30fa62428315a481428d84f701565b5f0bb96e
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-08 13:15:58 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,8 +3,8 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on T2 branch.
-// Program to convert input number to words and roman numerals.
+// Program on main branch.
+// T2: Program to convert input number to words and roman numerals.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
@@ -19,6 +19,7 @@ internal class Program {
             switch (ReadKey (true).Key) {
                case ConsoleKey.A: WriteLine (ConvertToWords (output)); break;
                case ConsoleKey.B: WriteLine (ConvertToRoman (output)); break;
+               default: WriteLine ("Please press 'A' or 'B'!"); break;
             }
          } else WriteLine ("Please enter a positive integer!");
          WriteLine ("\nPress 'Y' to continue");
@@ -44,16 +45,16 @@ internal class Program {
                   output += ones[quotient / 100 - 1] + " Hundred " + (num % 100 == 0 ? "" : "and ");
                   quotient %= 100; break;
                case >= 1 and < 10:
-                  output += ones[quotient - 1];
+                  output += ones[quotient - 1] + " ";
                   quotient = 0; break;
                case >= 11 and <= 19:
-                  output += teens[quotient - 11];
+                  output += teens[quotient - 11] + " ";
                   quotient = 0; break;
                default:
                   output += tens[quotient / 10 - 1] + " ";
                   quotient %= 10; break;
             }
-         if (num / baseVal > 0) output += " " + baseVals[indexPos] + " ";
+         if (num / baseVal > 0) output += baseVals[indexPos] + " ";
          num %= baseVal;
       }
       return output;
@@ -62,13 +63,14 @@ internal class Program {
    /// <summary>Converts input into roman numeral</summary>
    public static string ConvertToRoman (int num) {
       if (num < 1 || num > 3999) return "Number is out of range for conversion (1 - 3999)";
-      (int, string)[] numerals = [ (1000, "M"), (900, "CM"), (500, "D"), (400,"CD"), (100,"C"),
-            (90,"XC"), (50,"L"), (40,"XL"), (10,"X"), (9,"IX"), (5,"V"), (4,"IV"), (1,"I") ];
+      Dictionary<int, string> numerals = new () { { 1000, "M" }, { 900, "CM" }, { 500, "D" },
+         { 400, "CD" }, { 100, "C" }, { 90, "XC" }, { 50, "L" }, { 40, "XL" }, { 10, "X" },
+         { 9, "IX" }, { 5, "V" }, { 4, "IV" }, { 1, "I" } };
       string output = "";
-      for (int i = 0; num >= 0 && i < numerals.Length; i++)
-         while (num >= numerals[i].Item1) {
-            output += numerals[i].Item2;
-            num -= numerals[i].Item1;
+      foreach (var key in numerals.Keys)
+         while (num >= key) {
+            output += numerals[key];
+            num -= key;
          }
       return output;
    }
