015f808d315c036f19dd0f61b5b093c8221254f1
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-08 14:56:20 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,8 +3,8 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on T9 branch.
-// Program to reduce string by removing pairs of adjacent letters.
+// Program on main branch.
+// T9: Program to reduce string by removing pairs of adjacent letters.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
@@ -15,23 +15,24 @@ internal class Program {
          Clear ();
          Write ("Enter input: ");
          var input = ReadLine ();
-         if (!String.IsNullOrEmpty (input) && input.All (a => char.IsLetter (a)))
-            WriteLine ("Output: " + ReducedString (input));
+         if (!string.IsNullOrEmpty (input) && input.All (char.IsLetter))
+            WriteLine ("Output: " + Reduce (input));
          else WriteLine ("Please enter a valid input!");
          Write ("Press 'Y' to continue");
-
       } while (ReadKey (true).Key == ConsoleKey.Y);
    }
 
-   static string ReducedString (string input) {
-      int i = 0;
+   static string Reduce (string str) {
+      int i = 0, len = str.Length;
       string output = "";
       // Process input string up to second last character.
-      while (i < input.Length - 1) {
-         if (input[i] == input[i + 1]) i += 2;
-         else output += input[i++];
+      while (i < len - 1) {
+         // Increment index to exclude pairs of adjacent characters.
+         if (str[i] == str[i + 1]) i += 2;
+         else output += str[i++];
       }
-      if (i == input.Length - 1) output += input[i];
+      // Include last character in output if updated index is within bounds of array.
+      if (i == len - 1) output += str[i];
       return output;
    }
 }
