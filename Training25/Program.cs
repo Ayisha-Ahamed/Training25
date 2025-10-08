0ce8a96492f44efb841ee050d1661b050eb267c5
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-08 17:59:51 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,8 +3,8 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on T6 branch.
-// Program to find the digital root of a number.
+// Program on main branch.
+// T6: Program to find the digital root of a number.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
@@ -13,7 +13,7 @@ internal class Program {
    static void Main () {
       do {
          Clear ();
-         Write ("Enter input: ");
+         Write ("Enter any positive integer: ");
          if (int.TryParse (ReadLine (), out int num) && num >= 0)
             WriteLine ($"Digital root of {num} is {DigitalRoot (num)}");
          else WriteLine ("Please enter a positive integer!");
@@ -23,8 +23,7 @@ internal class Program {
 
    /// <summary>Returns the digital root of the input.</summary>
    static int DigitalRoot (int num) {
-      int sum = num;
-      while (sum > 9) sum = sum.ToString ().ToCharArray ().Sum (x => x - '0');
-      return sum;
+      while (num > 9) num = num.ToString ().Sum (x => x - '0');
+      return num;
    }
 }
