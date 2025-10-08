4bde3a5947a6483f09ac165b82df27875d69906b
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-08 11:00:11 +0530

    T12: Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -4,10 +4,28 @@
 // ------------------------------------------------------------------
 // Program.cs
 // Program on main branch.
+// T12: Program to find winner of voting contest.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      do {
+         Clear ();
+         Write ("Enter input: ");
+         var input = ReadLine ();
+         if (!string.IsNullOrEmpty (input) && input.ToLower ().All (x => x >= 'a' && x <= 'z'))
+            WriteLine (FindPerson (input).person + ", " + FindPerson (input).votes);
+         else WriteLine ("Please enter a valid string of characters");
+         Write ("Press 'Y' to continue");
+      } while (ReadKey (true).Key == ConsoleKey.Y);
+   }
+
+   static (char person, int votes) FindPerson (string input) {
+      var person = input.ToLower ().GroupBy (x => x)
+         .Select (a => new { a.Key, Count = a.Count () })
+         .OrderBy (a => a.Count).Last ();
+      return (char.ToUpper (person.Key), person.Count);
    }
 }
