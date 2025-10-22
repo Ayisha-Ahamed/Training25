7f1d064e296a4663a5461c41dba85c7f2c44a9bf
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-22 19:08:36 +0530

    Review fix 1

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -6,49 +6,39 @@
 // Program that prints sorted array by keeping the elements matching special character S
 // to the last of the array
 // ------------------------------------------------------------------------------------------------
+using System.Text.RegularExpressions;
 using static System.Console;
+
 namespace Training25;
 
 internal class Program {
+
    static void Main () {
       do {
          Clear ();
-         Write ("Enter array of letters: ");
-         var (str, isValid) = GetArray ();
-         if (isValid) {
-            Write ("Enter special character: ");
-            var splChar = GetSplChar ();
-            WriteLine ("\nPress 'D' to sort in descending order");
-            WriteLine (SortAndSwap (str, splChar, ReadKey (true).Key == ConsoleKey.D));
-         } else WriteLine (str);
+         Write ("Enter string: ");
+         var input = ReadLine () ?? "";
+         if (!string.IsNullOrEmpty (input) &&
+            Regex.IsMatch (input, @"\(\[(.*?)\], \w(, ""(ascending|descending)"")?\)")) {
+            input = input[2..^1];
+            var str = input.Replace (", ", "").Split (']');
+            var arr = str[0];
+            if (arr.All (char.IsLetter)) {
+               char spl = str[1][0]; string order = str[1][1..];
+               WriteLine (SortAndSwap (arr, spl, order == "\"descending\""));
+            } else WriteLine ("Invalid format! Please enter string of letters separated by \", \" " +
+               "inside the character array");
+         } else
+            WriteLine ("""
+               Invalid Format! Please ensure the input is of the following format:
+               ([a, b, c, a, c, b, d], a, "descending")
+                         A             S        O
+               A - Character array - string of letters separated by comma + space
+               S - Special character
+               O - Sort order
+               """);
          Write ("Press 'Y' to continue");
       } while (ReadKey (true).Key == ConsoleKey.Y);
-   }
-
-   // Returns the special character to be added at the end of the string.
-   static char GetSplChar () {
-      var splChar = ReadKey ().KeyChar;
-      while (!char.IsLetter (splChar)) {
-         WriteLine ("\nPlease enter a valid character (A-Z).Enter special character: ");
-         splChar = ReadKey ().KeyChar;
-      }
-      return splChar;
-   }
-
-   // Returns the error message (or) extracted string from comma separated input.
-   static (string str, bool isValid) GetArray () {
-      var array = ReadLine ();
-      if (string.IsNullOrEmpty (array)) return ("Input cannot be empty!", false);
-      string str = "";
-      for (int i = 0, len = array.Length; i < len; i++) {
-         bool isEven = (i & 1) == 0;
-         // Check if even positions and odd positions of the array
-         // are filled with letters and ',' respectively.
-         if ((isEven && !char.IsLetter (array[i]) || (!isEven && array[i] != ',')))
-            return ("Invalid format! Please enter comma seperated string of letters", false);
-         else if (isEven) str += array[i];
-      }
-      return (str, true);
    }
 
    // Returns the sorted array of letters.
@@ -56,8 +46,8 @@ internal class Program {
    static string SortAndSwap (string str, char splChar, bool isDescending) {
       var arr = str.Where (a => a != splChar);
       int sCount = str.Length - arr.Count ();
-      var sArr = (arr.Count () == str.Length) ? "" :
-         +',' + string.Join (",", new string (splChar, sCount));
+      // If special character is not an element in the array.
+      var sArr = sCount == 0 ? "" : ',' + string.Join (",", Enumerable.Repeat (splChar, sCount));
       if (isDescending) return string.Join (",", arr.OrderDescending ()) + sArr;
       return string.Join (",", arr.Order ()) + sArr;
    }
