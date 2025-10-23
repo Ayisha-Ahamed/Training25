85767048777389c3ee756d6b19dda1cdbbd2ff42
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-23 13:49:30 +0530

    Review fix 1

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,8 +3,7 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
-// T12: Program to find winner of voting contest.
+// T12: Program to find the winner with the most votes.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
@@ -15,17 +14,14 @@ internal class Program {
          Clear ();
          Write ("Enter input: ");
          var input = ReadLine ();
-         if (!string.IsNullOrEmpty (input) && input.ToLower ().All (x => x >= 'a' && x <= 'z'))
-            WriteLine (FindPerson (input).person + ", " + FindPerson (input).votes);
-         else WriteLine ("Please enter a valid string of characters");
+         if (!string.IsNullOrEmpty (input) && input.All (char.IsLetter)) {
+            (char person, int votes) = FindWinner (input);
+            WriteLine ($"{char.ToUpper (person)}, {votes}");
+         } else WriteLine ("Please enter a valid string of characters");
          Write ("Press 'Y' to continue");
       } while (ReadKey (true).Key == ConsoleKey.Y);
    }
 
-   static (char person, int votes) FindPerson (string input) {
-      var person = input.ToLower ().GroupBy (x => x)
-         .Select (a => new { a.Key, Count = a.Count () })
-         .OrderBy (a => a.Count).Last ();
-      return (char.ToUpper (person.Key), person.Count);
-   }
+   static (char Person, int Vote) FindWinner (string input) => input.ToLower ().GroupBy (x => x)
+       .Select (a => (a.Key, a.Count ())).MaxBy (a => a.Item2);
 }
