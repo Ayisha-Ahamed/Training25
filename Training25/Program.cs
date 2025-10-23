9a156aa4d6700146b9d6caa2cd110effabd01e22
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-23 17:30:40 +0530

    Review fix 2: Updated description, IsArmstrong() method

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,8 +3,7 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
-// T11: Program to find if a number is an armstrong number.
+// T11: Program to print if the input number is an armstrong number.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
@@ -23,11 +22,7 @@ internal class Program {
 
    // Returns if the input number is an armstrong number.
    static bool IsArmstrong (int num) {
-      int sum = 0, quotient = num, pow = num.ToString ().Length;
-      while (quotient > 0) {
-         sum += (int)Math.Pow (quotient % 10, pow);
-         quotient /= 10;
-      }
-      return sum == num;
+      string str = num.ToString ();
+      return str.Select (a => (int)Math.Pow (a - '0', str.Length)).Sum () == num;
    }
 }
