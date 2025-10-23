fb492cfbc302df281b534a33276a88ca44cd5d1a
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-23 14:57:12 +0530

    Review fix 5

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,7 +3,6 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
 // T9: Program to reduce string by removing pairs of adjacent letters.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
@@ -25,11 +24,15 @@ internal class Program {
    // Returns reduced string by removing pairs of adjacent letters.
    static string Reduce (string str) {
       string result = "";
+      bool hasPairs = false;
       for (int i = 1, len = str.Length; i <= len; i++) {
          // Increment index if adjacent characters are equal.
-         if (i < len && str[i] == str[i - 1]) i++;
-         else result += str[i - 1];
+         if (i < len && str[i] == str[i - 1]) {
+            hasPairs = true;
+            i++;
+         } else result += str[i - 1];
       }
-      return result;
+      // Reduce string until adjacent pairs of letters are no longer found.
+      return !hasPairs ? result : Reduce (result);
    }
 }
