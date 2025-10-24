f569cda340b41c9e9312939e5f1079148df7293a
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-24 15:06:04 +0530

    Review fix 1: Fixed formatting

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,9 +3,10 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program returns the smallest number of steps to transform a number into identical digits.
+// T14: Program prints the smallest number of steps to transform a number into identical digits.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
+
 namespace Training25;
 
 internal class Program {
@@ -13,24 +14,26 @@ internal class Program {
       do {
          Clear ();
          Write ("Enter number: ");
-         if (int.TryParse (ReadLine (), out int num)) {
+         if (int.TryParse (ReadLine (), out int num) && num >= 0) {
             (int digit, int steps) = NTransform (num);
-            WriteLine ($"{num} -> {new string ((char)(digit + '0'), num.ToString ().Length)} "
-              + $"{steps} steps");
-         } else WriteLine ("Please enter integer input!");
+            WriteLine ($"{num} -> {new string ((char)(digit + '0'), num.ToString ().Length)}"
+              + $" {steps} steps");
+         } else WriteLine ("Please enter a valid positive integer!");
          Write ("Press 'Y' to continue");
       } while (ReadKey (true).Key == ConsoleKey.Y);
    }
 
    // Returns the number of transforms required to change a number into identical digits.
-   static (int digit, int steps) NTransform (int num) {
-      var dArr = num.ToString ().Select (a => a - '0').ToArray (); // Array of digits in num.
-      int nSteps = 1000, digit = 0; // Assuming the transform takes 1000 steps initially.
+   static (int Digit, int Steps) NTransform (int num) {
+      // Extract array of digits from num.
+      var dArr = num.ToString ().Select (a => a - '0').ToArray ();
+      // Assume the transform takes 1000 steps initially.
+      (int digit, int steps) = (0, 1000);
       for (int i = 0, len = dArr.Length; i < len; i++) {
          int temp = 0;
-         for (int j = 0; j < len; j++) temp += (Math.Abs ((dArr[i] - dArr[j])));
-         if (temp < nSteps) (nSteps, digit) = (temp, dArr[i]);
+         for (int j = 0; j < len; j++) temp += Math.Abs (dArr[i] - dArr[j]);
+         if (temp < steps) (steps, digit) = (temp, dArr[i]);
       }
-      return (digit, nSteps);
+      return (digit, steps);
    }
 }
