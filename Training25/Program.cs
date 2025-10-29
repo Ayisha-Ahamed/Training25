37dbddfe679cdbcfd6de566115842e8a6a125317
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-10-29 12:17:09 +0530

    Test2: Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,29 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Test2: Program to generate excel column names [1 - 16384].
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      do {
+         Write ("Enter positive integer: ");
+         if (int.TryParse (ReadLine (), out int num) && num > 0 && num <= 16384) WriteLine (Column (num));
+         else WriteLine ("Enter positive integer within range [ 1 - 16384]");
+         WriteLine ("Press 'Y' to continue");
+      } while (ReadKey (true).Key == ConsoleKey.Y);
+   }
+
+   static string Column (int sNo) {
+      int num = sNo - 1, quotient = num / 26, remainder = num % 26;
+      string prefix = "";
+      while (quotient > 0) {
+         prefix += (char)('A' + quotient % 26 - 1);
+         quotient /= 26;
+      }
+      return prefix + (char)('A' + remainder);
    }
 }
